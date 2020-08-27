1dd456eb6a09215b4783e04285f57b0d4b3398a8
Maurice Perry <maurice@perry.productions>
2020-08-27 18:21:01 +0200

    Initial commit

--- app/src/main/java/org/tastefuljava/noodleauth/MainActivity.java
+++ app/src/main/java/org/tastefuljava/noodleauth/MainActivity.java
@@ -1,5 +1,7 @@
 package org.tastefuljava.noodleauth;
 
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,6 +17,8 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,19 +51,102 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.add_account);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(this::addAccount);
         accountListView = findViewById(R.id.account_listview);
         accountListAdapter = new AccountListAdapter(this);
         accountListView.setAdapter(accountListAdapter);
-        accountListAdapter.add(new Account("Digit",
-                Codec.BASE32.decode("MNYUWY2PIVYHAVCB"), 6, 30));
         handler.post(refresh);
+    }
+
+    private void addAccount(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final View dlgView = getLayoutInflater().inflate(R.layout.account_dialog,null);
+        Button btnCancel = (Button)dlgView.findViewById(R.id.cancel);
+        Button btnOkay = (Button)dlgView.findViewById(R.id.ok);
+        builder.setView(dlgView);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        btnCancel.setOnClickListener((v) -> alertDialog.dismiss());
+        btnOkay.setOnClickListener((v) -> {
+            EditText txtName = (EditText)dlgView.findViewById(R.id.account_name);
+            EditText txtKey = (EditText)dlgView.findViewById(R.id.account_key);
+            EditText txtOtpLength = (EditText)dlgView.findViewById(R.id.account_otplength);
+            EditText txtValidity = (EditText)dlgView.findViewById(R.id.account_validity);
+            accountListAdapter.add(new Account(txtName.getText().toString(),
+                    Codec.BASE32.decode(txtKey.getText().toString()),
+                    Integer.parseInt(txtOtpLength.getText().toString()),
+                    Integer.parseInt(txtValidity.getText().toString())));
+            writeState();
+            alertDialog.dismiss();
+        });
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(refresh);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        readState();
+        if (accountListAdapter.isEmpty()) {
+            accountListAdapter.add(new Account("Digit",
+                    Codec.BASE32.decode("MNYUWY2PIVYHAVCB"), 6, 30));
+        }
+    }
+
+    private void readState() {
+        SharedPreferences settings = getSharedPreferences(getString(R.string.prefs), MODE_PRIVATE);
+        int count = settings.getInt(getString(R.string.account_count), 0);
+        accountListAdapter.clear();
+        for (int i = 0; i < count; ++i) {
+            Account acc = readAccount(i, settings);
+            accountListAdapter.add(acc);
+        }
+    }
+
+    private Account readAccount(int i, SharedPreferences settings) {
+        String pfx = getString(R.string.account_prefix);
+        String name = settings.getString( pfx + i + getString(R.string.account_name), "");
+        assert name != null;
+        String key32 = settings.getString( pfx + i + getString(R.string.account_key), "");
+        assert key32 != null;
+        return new Account(name,
+                Codec.BASE32.decode(key32),
+                settings.getInt(pfx + i + getString(R.string.account_otplength), 6),
+                settings.getInt(pfx + i + getString(R.string.account_validity), 30));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writeState();
+    }
+
+    private void writeState() {
+        SharedPreferences settings = getSharedPreferences(getString(R.string.prefs), MODE_PRIVATE);
+        SharedPreferences.Editor edit = settings.edit();
+        int count = 0;
+        for (int i = 0; i < accountListAdapter.getCount(); ++i) {
+            Account acc = accountListAdapter.getItem(i);
+            if (acc != null) {
+                writeAccount(count, acc, edit);
+                edit.putInt(getString(R.string.account_count), ++count);
+            }
+        }
+        edit.apply();
+    }
+
+    private void writeAccount(int i, Account acc, SharedPreferences.Editor edit) {
+        String pfx = getString(R.string.account_prefix);
+        edit.putString(pfx + i + getString(R.string.account_name), acc.getName());
+        edit.putString(pfx + i + getString(R.string.account_key),
+                Codec.BASE32.encode(acc.getKey()));
+        edit.putInt(pfx + i + getString(R.string.account_otplength), acc.getOtpLength());
+        edit.putInt(pfx + i + getString(R.string.account_validity), acc.getValidity());
     }
 
     @Override
